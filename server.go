f2b80844db24e22f0d0f40e293da225ac2eba2c3
Alexey Elizarov <felex-ae@ya.ru>
2014-10-09 19:26:42 +0300

    clear fucking stuff

--- server.go
+++ server.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "time"
   "log"
-  "net/http"
-  "path"
-  "strconv"
 
   "github.com/go-martini/martini"
-  "github.com/igm/pubsub"
-  "github.com/carbocation/go-instagram/instagram"
-  "gopkg.in/igm/sockjs-go.v2/sockjs"
+  
   gooauth2 "github.com/golang/oauth2"
   "github.com/martini-contrib/oauth2"
   "github.com/martini-contrib/sessions"
 )
-
-var chat pubsub.Publisher
 
 // Returns a new Instagram OAuth 2.0 backend endpoint.
 func Instagram(opts *gooauth2.Options) martini.Handler {
@@ -28,20 +18,7 @@ func Instagram(opts *gooauth2.Options) martini.Handler {
 }
 
 func main() {
-  go check()
-
-  handler := sockjs.NewHandler("/echo", sockjs.DefaultOptions, echoHandler)
-
   m := martini.Classic();
-  m.NotFound(func(w http.ResponseWriter, r *http.Request) {
-    // Only rewrite paths *not* containing filenames
-    if path.Ext(r.URL.Path) == "" {
-      http.ServeFile(w, r, "public/index.html")
-    } else {
-      w.WriteHeader(http.StatusNotFound)
-      w.Write([]byte("404 page not found"))
-    }
-  })
   m.Use(sessions.Sessions("my_session", sessions.NewCookieStore([]byte("secret123"))))
   m.Use(Instagram(&gooauth2.Options{
     ClientID: "04df7554ee08464da70ee2530cc84774",
@@ -56,73 +33,9 @@ func main() {
     }
     c.Next()
   })
-
-  m.Use(martini.Logger())
-  m.Use(martini.Recovery())
-  m.Use(martini.Static("public"))
-
-/*
-  m.Get("/", oauth2.LoginRequired, func(tokens oauth2.Tokens) string {
-    http.ServeFile(w, r, "public/index.html")
-  })*/
-
-  /*
   m.Get("/", func() string {
     return "Hello world!"
   })
-  */
-
-  http.Handle("/echo/", handler)
-  http.Handle("/", m)
-  http.ListenAndServe(":3000", nil)
+  m.Run()
 }
 
-
-func echoHandler(session sockjs.Session) {
-  log.Println("new sockjs session established")
-  var closedSession = make(chan struct{})
-  chat.Publish("[info] new participant joined chat")
-  defer chat.Publish("[info] participant left chat")
-  go func() {
-  	reader, _ := chat.SubChannel(nil)
-  	for {
-  		select {
-  		case <-closedSession:
-  			return
-  		case msg := <-reader:
-  			if err := session.Send(msg.(string)); err != nil {
-  				return
-  			}
-  		}
-
-  	}
-  }()
-  for {
-  	if msg, err := session.Recv(); err == nil {
-  		chat.Publish(msg)
-  		continue
-  	}
-  	break
-  }
-  close(closedSession)
-  log.Println("sockjs session closed")
-}
-
-
-func check() {
-  // You can optionally pass your own HTTP's client, otherwise pass it with nil.
-  client := instagram.NewClient(nil)
-  client.ClientID = "8f2c0ad697ea4094beb2b1753b7cde9c"
-
-  //wait around with a forever loop
-  for {
-    time.Sleep(1 * time.Second)
-
-    media, err := client.Media.Get("822463308874820376")
-    if err != nil {
-      fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-    }
-    log.Println(media.Likes.Count)
-    chat.Publish(strconv.Itoa(media.Likes.Count))
-  }
-}
